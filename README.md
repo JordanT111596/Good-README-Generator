6d7ef7a840ef0795be9749368ddc727ea7b459ab
Jordan Triplett <JordanT111596@gmail.com>
2020-08-23 21:46:49 -0400

    Added base readme stuff, starter files, and added to question array

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Good-README-Generator
 Unit 09 Node.js and ES6+ Homework: Good README Generator
+
+Homework 7 by Jordan Triplett
+Completed 08/24/2020
+
+This assignment has us PLACEHOLDER.
+
+As specified in the assignment, PLACEHOLDER. Now CALL TO ACTION!
+
+This site can be found at https://jordant111596.github.io/Good-README-Generator
+
+![Image of Good README Generator](assets/Weather-Dashboard-Demo.gif?raw=true "Image of the Generator")
---
+++ index.js
@@ -0,0 +1,23 @@
+// array of questions for user
+const questions = ["What is your GitHub username?",
+    "What is your email address?",
+    "What is your project's title?",
+    "Please write a short description of your project",
+    "What kind of license should your project have?",
+    "What command should be run to install dependencies?",
+    "What command should be run to run tests?",
+    "What does the user need to know about using the repo?",
+    "What does the user need to know about contributing to the repo?"
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
