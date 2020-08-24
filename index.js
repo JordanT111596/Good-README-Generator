a6c837a15ac8382d7a3a469f14edc7d443113e4b
Jordan Triplett <JordanT111596@gmail.com>
2020-08-24 15:23:41 -0400

    Structured the questions for inquirer

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
+var inquirer = require("inquirer");
+
 // array of questions for user
-const questions = ["What is your GitHub username?",
-    "What is your email address?",
-    "What is your project's title?",
-    "Please write a short description of your project",
-    "What kind of license should your project have?",
-    "What command should be run to install dependencies?",
-    "What command should be run to run tests?",
-    "What does the user need to know about using the repo?",
-    "What does the user need to know about contributing to the repo?"
+const questions = [
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is your project's title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Please write a short description of your project",
+        name: "description"
+    },
+    {
+        type: "list",
+        message: "What kind of license should your project have?",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        default: "npm i",
+        name: "install"
+    },
+    {
+        type: "input",
+        message: "What command should be run to run tests?",
+        default: "npm test",
+        name: "test"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about using the repo?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to the repo?",
+        name: "contribute"
+    }
 ];
 
 // function to write README file
